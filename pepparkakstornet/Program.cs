dd93ad8b088e09333475c9f8f0d8a8863ab9f5f1
AndreasDahlgren <andreas.dahlgren@omegapoint.se>
2025-12-05 11:03:47 +0100

    push push

--- pepparkakstornet/Program.cs
+++ pepparkakstornet/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 class Team
 {
@@ -20,6 +21,10 @@ class Program
 
     static void Main(string[] args)
     {
+        // Configurable bonus settings
+        int bonusThreshold = 50; // Minimum height for bonus
+        int bonusValue = 50;     // Bonus points
+
         Console.WriteLine("PEPPARKAKSTORNET - RESULTATGENERATOR\n");
         string file = "scores.txt";
         if (!File.Exists(file))
@@ -51,7 +56,7 @@ class Program
             var team = new Team
             {
                 Name = parts[0].Trim(),
-                Height1 = height1,
+                Height1 = height1,  
                 DesignScore = designScore,
                 Hits = hits,
                 Height2 = height2
@@ -59,12 +64,12 @@ class Program
 
             // Del 1
             int heightScore = team.Height1;
-            if (team.Height1 > 50) heightScore += 50;
+            if (team.Height1 > bonusThreshold) heightScore += bonusValue;
             team.Del1Score = heightScore + team.DesignScore;
 
             // Del 2
             int duelScore = team.Hits * 20 + team.Height2;
-            if (team.Height2 > 50) duelScore += 50;
+            if (team.Height2 > bonusThreshold) duelScore += bonusValue;
             team.Del2Score = duelScore;
 
             team.TotalScore = team.Del1Score + team.Del2Score;
@@ -80,7 +85,16 @@ class Program
         var sorted = teams.OrderByDescending(t => t.TotalScore).ToList();
         var winner = sorted.First();
 
+        // Fun suspense animation before showing the winner
         Console.WriteLine();
+        Console.Write("Vinnaren räknas ut");
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(400);
+        }
+        Console.WriteLine("\n");
+
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("***************************************");
         Console.WriteLine($"***  VINNARE: {winner.Name} med {winner.TotalScore} poäng!  ***");
@@ -90,14 +104,14 @@ class Program
         Console.WriteLine("\nSlutresultat (sorterat på totalpoäng):");
         foreach (var t in sorted)
         {
-            int heightBonus1 = t.Height1 > 50 ? 50 : 0;
+            int heightBonus1 = t.Height1 > bonusThreshold ? bonusValue : 0;
             Console.WriteLine($"\n{t.Name}:");
             Console.WriteLine($"  Del 1 (Byggmoment):");
             Console.WriteLine($"    Höjd: {t.Height1} cm = {t.Height1}p{(heightBonus1 > 0 ? $" + {heightBonus1}p bonus" : "")}");
             Console.WriteLine($"    Designpoäng: {t.DesignScore}p");
             Console.WriteLine($"    Summa Del 1: {t.Del1Score}p");
 
-            int heightBonus2 = t.Height2 > 50 ? 50 : 0;
+            int heightBonus2 = t.Height2 > bonusThreshold ? bonusValue : 0;
             int hitPoints = t.Hits * 20;
             Console.WriteLine($"  Del 2 (Duellmoment):");
             Console.WriteLine($"    Träffar: {t.Hits} st = {hitPoints}p");
@@ -108,18 +122,70 @@ class Program
         }
 
         Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
+
+        while (!Console.KeyAvailable)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("***************************************");
+            Console.WriteLine($"***  VINNARE: {winner.Name} med {winner.TotalScore} poäng!  ***");
+            Console.WriteLine("***************************************");
+            Console.ResetColor();
+            Thread.Sleep(250);
+            Console.SetCursorPosition(0, Console.CursorTop - 3);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("***************************************");
+            Console.WriteLine($"***  VINNARE: {winner.Name} med {winner.TotalScore} poäng!  ***");
+            Console.WriteLine("***************************************");
+            Console.ResetColor();
+            Thread.Sleep(250);
+            Console.SetCursorPosition(0, Console.CursorTop - 3);
+        }
+        Console.ReadKey(true); // Clear the key press
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("***************************************");
         Console.WriteLine($"***  VINNARE: {winner.Name} med {winner.TotalScore} poäng!  ***");
         Console.WriteLine("***************************************");
         Console.ResetColor();
 
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine("Ej sorterad Lista (originalordning):");
-        foreach (var t in teams)
-        {
-            Console.WriteLine($"{t.Name}: Del 1 = {t.Del1Score}p, Del 2 = {t.Del2Score}p, Totalt = {t.TotalScore}p");
-        }
+        //Console.WriteLine();
+        //Console.WriteLine();
+        //Console.WriteLine("Ej sorterad Lista (originalordning):");
+        //foreach (var t in teams)
+        //{
+        //    Console.WriteLine($"{t.Name}: Del 1 = {t.Del1Score}p, Del 2 = {t.Del2Score}p, Totalt = {t.TotalScore}p");
+        //}
     }
 }
